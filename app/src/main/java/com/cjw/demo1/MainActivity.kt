8922b8c34ed747677fc9acb4a937fa56ea4bf06f
chenjiawei <chenjiawei@iboxpay.com>
2019-03-23 22:43:20 +0800

    LiveData中observe与observeForever使用

--- app/src/main/java/com/cjw/demo1/MainActivity.kt
+++ app/src/main/java/com/cjw/demo1/MainActivity.kt
@@ -21,12 +21,24 @@ class MainActivity : AppCompatActivity() {
         lifecycle.addObserver(mMainLifecycleObserver)
 
         mActivityLiveData = MutableLiveData()
-        // 与 Lifecycle 不同的是, 只有当 Observer 处于活跃的状态时才会进行回调, 否则内部是直接 return 处理的
+
+        // 与 Lifecycle 不同的是
+        // 当调用 observe 方法, 只有当 owner 处于活跃的状态时才会回调 Observer, 否则内部是直接 return 处理的
         // 参考 LifecycleBoundObserver
-        // 在 Activity 中, 活跃状态位于 onStart 之后, onStop之前
+
+        // 推荐使用 observe
+        // 这个例子中, owner 为 this, 即 Activity 对象
+        // 在 Activity 中, 活跃状态位于 onStart 之后, onStop 之前
         mActivityLiveData.observe(this, Observer { value ->
             Log.getSingleInstance().info("LiveData 收到当前的状态为 $value")
         })
+
+        // 如果调用的是 observeForever, 则都会回调 Observer
+        // 参考 AlwaysActiveObserver
+//        mActivityLiveData.observeForever { value ->
+//            Log.getSingleInstance().info("LiveData 收到当前的状态为 $value")
+//        }
+
         mActivityLiveData.value = "onCreate"
     }
 
